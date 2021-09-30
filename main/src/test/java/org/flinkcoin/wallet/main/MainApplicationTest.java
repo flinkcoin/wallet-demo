c639b08ca4faa78875b7c8c7f04386919eb037c0
corapeus <corapues@gmail.com>
2021-09-30 23:26:10 +0200

    update license, cleanup

--- main/src/test/java/org/flinkcoin/wallet/main/MainApplicationTest.java
+++ main/src/test/java/org/flinkcoin/wallet/main/MainApplicationTest.java
@@ -1,11 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.flinkcoin.wallet.main;
 
-import org.flinkcoin.wallet.main.MainApplication;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,10 +7,6 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- *
- * @author matej
- */
 public class MainApplicationTest {
     
     public MainApplicationTest() {

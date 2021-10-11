2085f32c251192ae817c42c9b777b01a384c8abe
corapeus <corapues@gmail.com>
2021-10-11 19:35:20 +0200

    removed license headers

--- main/src/main/java/org/flinkcoin/wallet/main/resources/wallet/WalletDtl.java
+++ main/src/main/java/org/flinkcoin/wallet/main/resources/wallet/WalletDtl.java
@@ -1,25 +1,5 @@
 package org.flinkcoin.wallet.main.resources.wallet;
 
-/*-
- * #%L
- * Wallet - Main
- * %%
- * Copyright (C) 2021 Flink Foundation
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
- */
-
 import org.flinkcoin.data.proto.common.Common.Block;
 import org.flinkcoin.data.proto.common.Common.Block.BlockType;
 import org.flinkcoin.helper.helpers.Base32Helper;
